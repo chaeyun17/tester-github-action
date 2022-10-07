305b3bba2e72fbd9028c3bfcfebb7edd5b551b6f
benji <benji@parti.coop>
2022-10-07 13:22:49 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/Test01.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test01 {
+
+  @Test
+  void test01_success(){
+    assertEquals("test", "test");
+  }
+
+  @Test
+  void test02_failed(){
+    assertEquals("test", "failedTest");
+  }
+}
